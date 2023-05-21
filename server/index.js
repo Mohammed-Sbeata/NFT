609a05769935a28b6f09ec283dad0449ebcb37d2
abeerAyad <abeerayyad2000@gmail.com>
2023-05-21 23:42:57 +0300

    save code

--- server/index.js
+++ server/index.js
@@ -1,7 +1,1 @@
-const express = require('express');
 
-const app = express();
-app.listen(3000, () => {
-  // eslint-disable-next-line no-console
-  console.log('Server Running on  http://localhost:3000');
-});

74087553cd96c305d4a4c66fbbfb5e7e0ea4274f
Basel AlSharif <basel.lo0o87@gmail.com>
2023-05-21 16:36:33 +0300

    db edit

--- server/db/config/build.sql
+++ server/db/config/build.sql
@@ -1,28 +1,26 @@
 BEGIN;
-DROP TABLE IF EXISTS users, products,carts CASCADE;
+DROP TABLE IF EXISTS users, products, cart CASCADE;
 
 CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(50) NOT NULL,
     image TEXT,
-    budget NUMERIC(10,2)
+    budget NUMERIC(10,2) NOT NULL
 );
 CREATE TABLE products(
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL UNIQUE,
-    image TEXT ,
-    floor FLOAT,
-    userId int ,
-    FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+    image TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL
 );
 CREATE TABLE cart(
-    id SERIAL PRIMARY KEY,
-    userId int ,
-    productId int,
-    FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
-    FOREIGN KEY (productId) REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    id BIGSERIAL PRIMARY KEY,
+    userId INT NOt NULL,
+    productId INT NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (productId) REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 COMMIT;

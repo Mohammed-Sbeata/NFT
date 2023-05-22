dbc3694595cb77fb89ba5110f19440f367d18c6b
Nour-Agha99 <temoagha@gmail.com>
2023-05-22 10:02:38 +0300

    create react landingPAge

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,300 @@
+import "./main.css";
+import img from "../assets/images/2aa457150547103.62fc32b4e21a7.jpg";
+import certified from "../assets/images/certified.png";
+
+const Main = () => {
+  return (
+    <main>
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M ETH</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+      <div className="card">
+        <img alt="image" src={img} />
+        <name>
+          Wellcome to title
+          <img className="certified" src={certified} />
+        </name>
+        <discription>
+          <right>
+            <label>Volume</label>
+            <span>13M USD</span>
+          </right>
+          <left>
+            <label>Floor Price</label>
+            <span>150 USD</span>
+          </left>
+        </discription>
+      </div>
+      {/*  */}
+    </main>
+  );
+};
+
+export default Main;

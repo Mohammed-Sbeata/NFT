2ae3d24d2fe83bacfd9410f06241d22f463de04d
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-05-23 10:35:22 +0300

    create fake data again relates #22

---
+++ server/db/queries/getUserPassword.js
@@ -0,0 +1,11 @@
+const connection = require('../config/connection');
+
+const getUserPassword = (username) => {
+  sql = {
+    text: 'SELECT id, username, password  FROM users WHERE username = $1;',
+    values: [username],
+  };
+  return connection.query(sql);
+};
+
+module.exports = getUserPassword;
